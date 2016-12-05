173dbc305d7307a71fc44590700c705cacb7c3a4
jimting <handsome841206@gmail.com>
2016-12-05 20:16:10 +0800

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,33 @@
+﻿<?php session_start(); ?>
+<!--上方語法為啟用session，此語法要放在網頁最前方-->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<?php
+//連接資料庫
+//只要此頁面上有用到連接MySQL就要include它
+include("mysql_connect.inc.php");
+$id = $_POST['id'];
+$pw = $_POST['pw'];
+
+//搜尋資料庫資料
+$sql = "SELECT * FROM user where user_number = '$id'";
+$result = mysql_query($sql);
+$row = @mysql_fetch_row($result);
+
+//判斷帳號與密碼是否為空白
+//以及MySQL資料庫裡是否有這個會員
+if($id != null && $pw != null && $row[1] == $id && $row[2] == $pw)
+{
+        //將帳號寫入session，方便驗證使用者身份
+        $_SESSION['user_number'] = $id;
+        echo '<div class="alert alert-success"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a><strong>Success!</strong>登入成功囉！系統將自動轉跳自首頁！</div>';
+        echo '<meta http-equiv=REFRESH CONTENT=1;url=index.html>';
+}
+else
+{
+		echo '<div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a><strong>Error!</strong>登入失敗！請再次確認您的帳號密碼是否正確<br>系統將轉跳自登入畫面！</div>';
+        echo '<meta http-equiv=REFRESH CONTENT=1;url=login.html>';
+}
+?>
---
+++ logout.php
@@ -0,0 +1,8 @@
+﻿<?php session_start(); ?>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<?php
+//將session清空
+unset($_SESSION['user_number']);
+echo '登出中......';
+echo '<meta http-equiv=REFRESH CONTENT=1;url=login.html>';
+?>
