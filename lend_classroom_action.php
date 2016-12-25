5593f435931d34df8a5a0733796cba32f95dbea6
jimting <handsome841206@gmail.com>
2016-12-25 14:57:54 +0800

    大更新
    一段時間忘記丟GH 來同步一下

---
+++ lend_classroom_action.php
@@ -0,0 +1,35 @@
+﻿<?php session_start(); ?>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<?php
+include("mysqli_connect.inc.php");
+
+$room_ID = $_POST['classroom'];
+$user_ID = $_SESSION['user_ID'];
+
+if($user_ID != null && $room_ID != null)
+{
+        //新增資料進資料庫語法
+		$datetime = date ("Y-m-d H:i:s" , mktime(date('H')+8, date('i'), date('s'), date('m'), date('d'), date('Y'))) ; 
+        $sql = "insert into lend_room (user_ID,room_ID,lend_date) values ('$user_ID','$room_ID','$datetime')";
+        if(mysqli_query($db,$sql))
+        {
+			$sql2 = "update classroom set room_status = 1 where room_ID = '$room_ID'";
+			if(mysqli_query($db,$sql2))
+			{
+				echo '借用教室成功!';
+			}
+			else
+			{
+				echo '借用教室失敗1!';
+			}				
+        }
+        else
+        {
+                echo '借用教室失敗2!';
+        }
+}
+else
+{
+        echo '您無權限觀看此頁面!';
+}
+?>
---
+++ return_equip.php
@@ -0,0 +1,41 @@
+﻿<?php session_start(); ?>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<form method="post" action = "return_equip_finish.php">
+    <div class="form-group">
+      <label for="equip">選擇器材:</label>
+      <select name="lend_equip_ID">
+		
+<?php
+include("mysqli_connect.inc.php");
+$user_ID = $_SESSION['user_ID'];//使用者
+
+$sql3 = "SELECT * FROM lend_equip WHERE user_ID = '$user_ID'"; //使用者所有借據
+$stmt = $db->query($sql3);
+$result3=mysqli_fetch_object($stmt);
+
+if($_SESSION['user_number'] != null)
+	{
+		$sql = "SELECT * FROM equipment,lend_equip WHERE equipment.equip_ID = lend_equip.equip_ID && lend_equip.user_ID = '$user_ID'";
+		//$sql = "SELECT equipment.equip_name,lend_equip.equip_ID,lend_equip.lend_equip_quan,lend_equip.lend_equip_ID FROM equipment, lend_equip WHERE lend_equip.equip_ID = equipment.equip_ID && user_ID = '$user_ID'";
+		if($stmt = $db->query($sql))
+		{
+			while($result=mysqli_fetch_object($stmt))
+			{
+					echo "<option value=".$result->lend_equip_ID.">".$result->equip_name."歸還".$result->lend_equip_quan."個</option>";
+										
+			}
+
+		}
+	echo '</select><br>
+    </div>
+    <button type="submit" class="btn btn-default">確認</button>
+	</form>
+	</div>';
+	}	
+	else
+	{
+		echo '你還沒登入';
+		echo '<meta http-equiv=REFRESH CONTENT=2;url=login.html>';
+	}
+	
+?>
